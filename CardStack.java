39324d14217059d381a48ec0920d8cd5e815d949
akloskowski <akloskowski@stuy.edu>
2018-01-11 13:28:21 -0500

    CardStack.Java

--- CardStack.java
+++ CardStack.java
@@ -3,4 +3,11 @@ import java.util.ArrayList;
 public abstract class CardStack {
     ArrayList<Card> cards = new ArrayList();
     public abstract Boolean canMove(Card c);
+    public Card addCard(Card c){
+	cards.add(c);
+	return c;
+    }
+    public Card removeCard(){
+	return cards.remove(cards.size());
+    }
 }
