9a9bb18002d70919f9004813550aa99e5f816c80
Max <mmillar@stuy.edu>
2018-01-17 00:34:42 -0500

    cards can be placed on empty spaces after my first bug fix

--- Cascade.java
+++ Cascade.java
@@ -1,6 +1,6 @@
 public class Cascade extends CardStack {
     public Boolean canMove(Card c){
 	//returns true if c is of descending rank and alternating color
-	return (this.getCard().getRank()).equals(c.getRank()+1)&&(((this.getCard().getSuit() %  2) == 0 && (c.getSuit() % 2) == 1) || ((this.getCard().getSuit() %  2) == 1 && (c.getSuit() % 2) == 0)) ;
+	return (this.isEmpty()) || (this.getCard().getRank()).equals(c.getRank()+1)&&(((this.getCard().getSuit() %  2) == 0 && (c.getSuit() % 2) == 1) || ((this.getCard().getSuit() %  2) == 1 && (c.getSuit() % 2) == 0)) ;
     }
 }
