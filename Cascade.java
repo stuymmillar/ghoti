ea7202219e095855f10f40a96c66efd885f29d36
Max <mmillar@stuy.edu>
2018-01-17 00:02:55 -0500

    Some more devlog stuff and the bug with canMove()

--- Cascade.java
+++ Cascade.java
@@ -1,6 +1,6 @@
 public class Cascade extends CardStack {
     public Boolean canMove(Card c){
 	//returns true if c is of descending rank and alternating color
-	return (this.getCard().getRank()).equals(c.getRank()+1)&&((this.getCard().getSuit().equals(0)||this.getCard().getSuit().equals(2))&&(c.getSuit().equals(1)||c.getSuit().equals(3)))||((this.getCard().getSuit().equals(1)||this.getCard().getSuit().equals(3))&&(c.getSuit().equals(0)||c.getSuit().equals(2)));
+	return (this.getCard().getRank()).equals(c.getRank()+1)&&((this.getCard().getSuit() %  2) == 0 && (c.getSuit() % 2) == 1);
     }
 }
